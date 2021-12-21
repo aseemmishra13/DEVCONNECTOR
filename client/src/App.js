da1ed425df9068eb13a6e7b3a0e54babb7ebc41d
aseemmishra13 <mishraaseem05@gmail.com>
2021-12-21 12:45:23 +0530

    Front end -> landing page,navbar,register,login

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import React, { Fragment} from 'react'
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+
+
+const App=()=> 
+<Router>
+    <Fragment>
+     
+      <Navbar/>
+      <Routes>
+      <Route exact path= "/" element = { <Landing />} />
+      </Routes>
+      <section className='container'>
+        
+          <Routes>
+            <Route exact path = "/register" element = {<Register />} />
+            <Route exact path = "/login" element = {<Login />} />
+          </Routes>
+      
+      </section>
+    </Fragment>
+</Router> 
+
+
+export default App;
